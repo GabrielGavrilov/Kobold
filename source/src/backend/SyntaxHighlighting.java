11dd2d1e69fb2f4337e859e10b602f8fa784933e
Gabriel Gavrilov <gabrielgavrilov11@gmail.com>
2023-02-06 21:25:52 -0600

    chaged syntax highlighting

--- source/src/backend/SyntaxHighlighting.java
+++ source/src/backend/SyntaxHighlighting.java
@@ -10,10 +10,8 @@ import java.util.regex.Pattern;
 public class SyntaxHighlighting {
 
     public static StyleContext context = StyleContext.getDefaultStyleContext();
-    public static AttributeSet blueAttribute = context.addAttribute(context.getEmptySet(), StyleConstants.Foreground, new Color(84,156,214,255));
-    public static AttributeSet greenAttribute = context.addAttribute(context.getEmptySet(), StyleConstants.Foreground, new Color(78,201,176,255));
-    public static AttributeSet pinkAttribute = context.addAttribute(context.getEmptySet(), StyleConstants.Foreground, new Color(197,134,192,255));
-    public static AttributeSet defaultAttribute = context.addAttribute(context.getEmptySet(), StyleConstants.Foreground, new Color(203,216,228,255));
+    public static AttributeSet attribute;
+
     public DefaultStyledDocument styleDocument = new DefaultStyledDocument() {
         public void insertString(int offset, String syntax, AttributeSet highlight) throws BadLocationException {
             super.insertString(offset, syntax, highlight);
@@ -32,27 +30,34 @@ public class SyntaxHighlighting {
                 if(wordRight == firstNonWord || String.valueOf(text.charAt(wordRight)).matches("\\W")) {
                     try {
                         Scanner keywordsScanner = new Scanner(keywordsFile);
-                        int currentLine = 1;
-                        while(keywordsScanner.hasNextLine()) {
-                            String[] keywords = keywordsScanner.nextLine().split(",");
-                            for(String keyword : keywords) {
-                                if(text.substring(wordLeft, wordRight).matches(keyword)) {
-                                    switch(currentLine) {
-                                        case 1:
-                                            setCharacterAttributes(wordLeft, wordRight - wordLeft, blueAttribute, false);
-                                            break;
-                                        case 2:
-                                            setCharacterAttributes(wordLeft, wordRight - wordLeft, greenAttribute, false);
-                                            break;
-                                        case 3:
-                                            setCharacterAttributes(wordLeft, wordRight - wordLeft, pinkAttribute, false);
-                                            break;
-                                    }
-                                    break;
-                                }
+                        while(keywordsScanner.hasNext()) {
+                            String currentKeyword = keywordsScanner.next();
+
+                            String keyword = currentKeyword.split("=")[0];
+                            String colour = currentKeyword.split("=")[1].toLowerCase();
+
+                            if(text.substring(wordLeft, wordRight).matches(keyword) && colour.equals("blue")) {
+                                attribute = context.addAttribute(context.getEmptySet(), StyleConstants.Foreground, new Color(84,156,214,255));
+                                setCharacterAttributes(wordLeft, wordRight - wordLeft, attribute, false);
+                                break;
                             }
-                            currentLine++;
+                            else if(text.substring(wordLeft, wordRight).matches(keyword) && colour.equals("green")) {
+                                attribute = context.addAttribute(context.getEmptySet(), StyleConstants.Foreground, new Color(78,201,176,255));
+                                setCharacterAttributes(wordLeft, wordRight - wordLeft, attribute, false);
+                                break;
+                            }
+                            else if(text.substring(wordLeft, wordRight).matches(keyword) && colour.equals("pink")) {
+                                attribute = context.addAttribute(context.getEmptySet(), StyleConstants.Foreground, new Color(197,134,192,255));
+                                setCharacterAttributes(wordLeft, wordRight - wordLeft, attribute, false);
+                                break;
+                            }
+                            else {
+                                attribute = context.addAttribute(context.getEmptySet(), StyleConstants.Foreground, new Color(203,216,228,255));
+                                setCharacterAttributes(wordLeft, wordRight - wordLeft, attribute, false);
+                            }
+
                         }
+
                     } catch(Exception e) {
                         e.printStackTrace();
                     }
