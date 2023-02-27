53f1db2a09bb3836e5b4b1272e670953117e8181
Gabriel Gavrilov <gabrielgavrilov11@gmail.com>
2023-02-26 22:18:46 -0600

    update

---
+++ src/core/KoboldColours.java
@@ -0,0 +1,15 @@
+package core;
+
+public class KoboldColours {
+    public enum Colours {
+        DARK,
+        GREY,
+        RED,
+        ORANGE,
+        PINK,
+        PURPLE,
+        GREEN,
+        BLUE,
+        TEAL
+    }
+}
