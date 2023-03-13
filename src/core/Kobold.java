a8ea131a1d0ff33c7795b5f1fe9f4962c6b1f21a
Gavrilov <gavrilov3398@saskpolytech.ca>
2023-03-13 11:56:11 -0600

    update

--- src/core/Kobold.java
+++ src/core/Kobold.java
@@ -4,6 +4,12 @@ import client.KoboldClient;
 
 public class Kobold {
     public static void main(String[] args) {
+
+        if(args.length < 1) {
+            System.out.println("[Error] : You must provide a directory for Kobold to open.");
+            System.exit(1);
+        }
+
         System.setProperty("user.dir", args[0]);
         KoboldClient client = new KoboldClient();
     }
